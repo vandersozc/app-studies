e159c0edfa544901ac0075843934a3bba5d0b9ac
Vanderson Campanholi <vando.zc@gmail.com>
2019-08-04 13:22:51 -0300

    Navigator widget

--- buscador_gifs/lib/main.dart
+++ buscador_gifs/lib/main.dart
@@ -1,1 +1,1 @@
-import 'package:buscador_gifs/ui/HomePage.dart';import 'package:flutter/material.dart';void main() {  runApp(MaterialApp(    home: HomePage(),    theme: ThemeData(hintColor: Colors.white),  ));}
+import 'package:buscador_gifs/ui/home_page.dart';import 'package:flutter/material.dart';void main() {  runApp(MaterialApp(    home: HomePage(),    theme: ThemeData(hintColor: Colors.white),  ));}
