d51550479f800910097bb6db26b8ec2cd1cb3c62
Vanderson Campanholi <vando.zc@gmail.com>
2019-08-21 00:14:33 -0300

    Scoped model

--- loja_virtual/lib/main.dart
+++ loja_virtual/lib/main.dart
@@ -11,20 +11,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScopedModel<UserModel>(
       model: UserModel(),
-      child: ScopedModelDescendant(
-        builder: (context, child, model) {
-          return ScopedModel<CartModel>(
-            model: CartModel(model),
-            child: MaterialApp(
-              title: 'Loja Virtual',
-              theme: ThemeData(
-                  primarySwatch: Colors.blue,
-                  primaryColor: Color.fromARGB(255, 4, 125, 141)),
-              debugShowCheckedModeBanner: false,
-              home: HomeScreen(),
-            ),
-          );
-        }
+      child: ScopedModelDescendant<UserModel>(
+          builder: (context, child, model){
+            return ScopedModel<CartModel>(
+              model: CartModel(model),
+              child: MaterialApp(
+                  title: "Flutter's Clothing",
+                  theme: ThemeData(
+                      primarySwatch: Colors.blue,
+                      primaryColor: Color.fromARGB(255, 4, 125, 141)
+                  ),
+                  debugShowCheckedModeBanner: false,
+                  home: HomeScreen()
+              ),
+            );
+          }
       ),
     );
   }
